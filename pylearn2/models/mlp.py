a2231a7e7545e178e92750e5765c8925b4cdfc5d
antoan2 <antoine.biard.10@gmail.com>
2014-09-03 14:03:15 -0400

    added the ConvmixLayer and Softmax_multidim layer

--- pylearn2/models/mlp.py
+++ pylearn2/models/mlp.py
@@ -5293,3 +5293,310 @@ class BadInputSpaceError(TypeError):
     An error raised by an MLP layer when set_input_space is given an
     object that is not one of the Spaces that layer supports.
     """
+
+
+class Softmax_multidim(Layer):
+    """
+    .. todo::
+
+        WRITEME (including parameters list)
+
+    Parameters
+    ----------
+    n_classes : WRITEME
+    layer_name : WRITEME
+    irange : WRITEME
+    istdev : WRITEME
+    sparse_init : WRITEME
+    W_lr_scale : WRITEME
+    b_lr_scale : WRITEME
+    max_row_norm : WRITEME
+    no_affine : WRITEME
+    max_col_norm : WRITEME
+    init_bias_target_marginals : WRITEME
+    """
+
+    def __init__(self, n_classes, layer_name, irange=None,
+                 istdev=None,
+                 sparse_init=None, W_lr_scale=None,
+                 b_lr_scale=None, max_row_norm=None,
+                 no_affine=True,
+                 max_col_norm=None, init_bias_target_marginals=None):
+
+        super(Softmax_multidim, self).__init__()
+
+        if isinstance(W_lr_scale, str):
+            W_lr_scale = float(W_lr_scale)
+
+        self.__dict__.update(locals())
+        del self.self
+        del self.init_bias_target_marginals
+        assert isinstance(n_classes, py_integer_types)
+        
+
+    @wraps(Layer.set_input_space)
+    def set_input_space(self, space):
+
+        self.input_space = space
+
+        self.output_space = VectorSpace(self.input_space.shape[0]*self.input_space.shape[1]*5)
+        
+        self.desired_space = self.input_space
+        if not isinstance(space, Space):
+            raise TypeError("Expected Space, got " +
+                            str(space)+" of type "+str(type(space)))
+
+        self.input_dim = space.get_total_dimension()
+        self.needs_reformat = not isinstance(space, Conv2DSpace)                                   
+        desired_dim = self.n_classes       
+        self._params = []        
+       
+    @wraps(Layer.fprop)
+    def fprop(self, state_below):
+
+        self.input_space.validate(state_below)       
+        self.desired_space.validate(state_below)
+        assert state_below.ndim == 4
+        assert self.mlp.batch_size == 128
+        Z = state_below
+        e_Z = T.exp(Z - Z.max(axis=0, keepdims=True))
+        rval = e_Z /e_Z.sum(axis=0, keepdims=True)
+        rval_swaped = rval.dimshuffle(3,1,2,0)
+        rval_swaped = rval_swaped.reshape(shape=(self.mlp.batch_size,self.input_space.shape[0]*self.input_space.shape[1]*5),ndim=2)              
+        return rval_swaped
+
+    @wraps(Layer.cost)
+    def cost(self, Y, Y_hat):
+
+        z = Y_hat
+        
+        z = z.reshape(shape=(self.mlp.batch_size*
+                                   self.input_space.shape[0]*
+                                   self.input_space.shape[1],
+                                   self.input_space.num_channels ),ndim=2)
+        
+        Y = Y.reshape(shape=(self.mlp.batch_size*
+                                   self.input_space.shape[0]*
+                                   self.input_space.shape[1],
+                                   self.input_space.num_channels ),ndim=2)
+
+        z = z - z.max(axis=1).dimshuffle(0, 'x')
+        log_prob = z - T.log(T.exp(z).sum(axis=1).dimshuffle(0, 'x'))
+        # we use sum and not mean because this is really one variable per row
+        log_prob_of = (Y * log_prob).sum(axis=1)
+        assert log_prob_of.ndim == 1
+        rval = log_prob_of.mean()
+        return  - rval
+
+    @wraps(Layer.get_layer_monitoring_channels)
+    def get_layer_monitoring_channels(self, state_below=None,
+                                    state=None, targets=None):
+
+        # channels that does not require state information
+        rval = OrderedDict()
+
+        if (state_below is not None) or (state is not None):
+            
+            state = state.reshape(shape=(self.mlp.batch_size*
+                                   self.input_space.shape[0]*
+                                   self.input_space.shape[1],
+                                   self.input_space.num_channels ),ndim=2)
+
+            mx = state.max(axis=1)   
+
+            rval.update(OrderedDict([('mean_max_class', mx.mean()),
+                                ('max_max_class', mx.max()),
+                                ('min_max_class', mx.min())]))
+
+            assert  targets is not None
+            targets = targets.reshape(shape=(self.mlp.batch_size*
+                                   self.input_space.shape[0]*
+                                   self.input_space.shape[1],
+                                   self.input_space.num_channels ),ndim=2)
+        
+            y_hat = T.argmax(state, axis=1)
+            y = T.argmax(targets, axis=1)
+            
+            #state = printing.Print('state')(state)
+            misclass = T.neq(y, y_hat).mean()
+            misclass = T.cast(misclass, config.floatX)
+            rval['misclass'] = misclass
+            
+            state = state.reshape(shape=(self.mlp.batch_size,
+                                   self.input_space.shape[0]*
+                                   self.input_space.shape[1]*
+                                   self.input_space.num_channels ),ndim=2)
+            targets = targets.reshape(shape=(self.mlp.batch_size,
+                                   self.input_space.shape[0]*
+                                   self.input_space.shape[1]*
+                                   self.input_space.num_channels ),ndim=2)
+
+            rval['nll'] = self.cost(Y_hat=state, Y=targets)
+        return rval
+
+    @wraps(Layer.get_weight_decay)
+    def get_weight_decay(self, coeff):
+        return 0
+
+    @wraps(Layer.get_l1_weight_decay)
+    def get_l1_weight_decay(self, coeff):
+        return 0
+
+class ConvmixLayer(Layer):
+    """
+    A wrapper around a different layer that flattens
+    the original layer's output.
+
+    The cost works by unflattening the target and then
+    calling the wrapped Layer's cost.
+
+    This is mostly intended for use with CompositeLayer as the wrapped
+    Layer, and is mostly useful as a workaround for theano not having
+    a TupleVariable with which to represent a composite target.
+
+    There are obvious memory, performance, and readability issues with doing
+    this, so really it would be better for theano to support TupleTypes.
+
+    See pylearn2.sandbox.tuple_var and the theano-dev e-mail thread
+    "TupleType".
+
+    Parameters
+    ----------
+    raw_layer : WRITEME
+        WRITEME
+    """
+
+    def __init__(self, raw_layer):
+        super(ConvmixLayer, self).__init__()
+        self.__dict__.update(locals())
+        del self.self
+        self.layer_name = raw_layer.layer_name
+
+    @wraps(Layer.set_input_space)
+    def set_input_space(self, space):
+
+        self.raw_layer.set_input_space(space)
+        sp1 = self.raw_layer.get_output_space().components[0]
+        sp2 = self.raw_layer.get_output_space().components[1]
+
+        nb_channels1 = sp1.num_channels
+        nb_channels2 = sp2.num_channels
+        
+        self.output_space = Conv2DSpace(shape=sp1.shape,
+                                             num_channels=nb_channels1+nb_channels2,
+                                         axes=('c', 0, 1, 'b')) 
+
+    @wraps(Layer.get_input_space)
+    def get_input_space(self):
+        return self.raw_layer.get_input_space()
+
+    @wraps(Layer.get_monitoring_channels)
+    def get_monitoring_channels(self, data):
+        return self.raw_layer.get_monitoring_channels(data)
+
+    @wraps(Layer.get_layer_monitoring_channels)
+    def get_layer_monitoring_channels(self, state_below=None,
+                                      state=None, targets=None):
+        return self.raw_layer.get_layer_monitoring_channels(
+            state_below=state_below,
+            state=state,
+            targets=targets
+            )
+
+    @wraps(Layer.get_monitoring_data_specs)
+    def get_monitoring_data_specs(self):
+        return self.raw_layer.get_monitoring_data_specs()
+
+    @wraps(Layer.get_params)
+    def get_params(self):
+        return self.raw_layer.get_params()
+
+    @wraps(Layer.get_weights)
+    def get_weights(self):
+        return self.raw_layer.get_weights()
+
+    @wraps(Layer.get_weight_decay)
+    def get_weight_decay(self, coeffs):
+        return self.raw_layer.get_weight_decay(coeffs)
+
+    @wraps(Layer.get_l1_weight_decay)
+    def get_l1_weight_decay(self, coeffs):
+        return self.raw_layer.get_l1_weight_decay(coeffs)
+
+    @wraps(Layer.set_batch_size)
+    def set_batch_size(self, batch_size):
+        self.raw_layer.set_batch_size(batch_size)
+
+    @wraps(Layer.censor_updates)
+    def censor_updates(self, updates):
+        self.raw_layer.censor_updates(updates)
+
+    @wraps(Layer.get_lr_scalers)
+    def get_lr_scalers(self):
+        return self.raw_layer.get_lr_scalers()
+
+    @wraps(Layer.fprop)
+    def fprop(self, state_below):
+
+        raw = self.raw_layer.fprop(state_below)
+
+        #return self.raw_layer.get_output_space().format_as(raw,
+        #                                                   self.output_space)
+        return theano.tensor.concatenate(raw, axis=0)
+
+    @wraps(Layer.cost)
+    def cost(self, Y, Y_hat):
+
+        raw_space = self.raw_layer.get_output_space()
+        target_space = self.output_space
+        raw_Y = target_space.format_as(Y, raw_space)
+
+        if isinstance(raw_space, CompositeSpace):
+            # Pick apart the Join that our fprop used to make Y_hat
+            assert hasattr(Y_hat, 'owner')
+            owner = Y_hat.owner
+            assert owner is not None
+            assert str(owner.op) == 'Join'
+            # first input to join op is the axis
+            raw_Y_hat = tuple(owner.inputs[1:])
+        else:
+            # To implement this generally, we'll need to give Spaces an
+            # undo_format or something. You can't do it with format_as
+            # in the opposite direction because Layer.cost needs to be
+            # able to assume that Y_hat is the output of fprop
+            raise NotImplementedError()
+        raw_space.validate(raw_Y_hat)
+
+        return self.raw_layer.cost(raw_Y, raw_Y_hat)
+
+    @wraps(Layer.set_mlp)
+    def set_mlp(self, mlp):
+
+        super(ConvmixLayer, self).set_mlp(mlp)
+        self.raw_layer.set_mlp(mlp)
+
+    @wraps(Layer.get_weights)
+    def get_weights(self):
+
+        return self.raw_layer.get_weights()
+
+    @wraps(Layer.dropout_fprop)
+    def dropout_fprop(self, state_below, default_input_include_prob=0.5,
+                      input_include_probs=None, default_input_scale=2.,
+                      input_scales=None, per_example=True, theano_rng=None):
+
+        if theano_rng is None:
+            theano_rng = MRG_RandomStreams(max(self.rng.randint(2 ** 15), 1))
+
+        raw = self.raw_layer.dropout_fprop(
+                      state_below,
+                      default_input_include_prob=default_input_include_prob,
+                      input_include_probs=input_include_probs,
+                      default_input_scale=default_input_scale,
+                      input_scales=input_scales,
+                      per_example=per_example,
+                      theano_rng=theano_rng
+                  )
+
+        return theano.tensor.concatenate(raw, axis=0)
+
